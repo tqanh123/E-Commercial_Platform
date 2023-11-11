aa82241fdddd58fed7f09f363d87cf71fbff72b2
thanhkhiem1411 <133770651+thanhkhiem1411@users.noreply.github.com>
2023-11-11 20:23:40 +0700

    Add files via upload

---
+++ CreateSchemaPDM.sql
@@ -0,0 +1,11 @@
+-- run each one in turn --
+
+-- Create Schema [Product]; --
+
+-- Create Schema [Cart]; -- 
+
+-- Create Schema [OrderManagement]; --
+
+-- Create Schema [HistoryManagement]; --
+
+-- Create Schema [Account]; -- 
