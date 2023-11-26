b447640680eada7f17754a2b27f64880be57d2de
thanhkhiem1411 <133770651+thanhkhiem1411@users.noreply.github.com>
2023-11-26 23:17:00 +0700

    Update PDM.sql

--- PDMSQL/PDM.sql
+++ PDMSQL/PDM.sql
@@ -14,15 +14,14 @@ CREATE TABLE [Account].[Account] (
 CREATE TABLE [Cart].[Cart] (
     Cart_ID INT PRIMARY KEY,
     Customer_ID INT,
-    Order_ID INT,
     Create_at DATETIME,
     Update_at DATETIME
-)
+    FOREIGN KEY (Customer_ID) REFERENCES [Account].[Customer],
+);
 
 CREATE TABLE [Product].[Category] (
     Category_ID INT PRIMARY KEY,
     Category_Name VARCHAR(255),
-    Category_Description TEXT
 ); 
 
 CREATE TABLE [Product].[Product] (
@@ -31,7 +30,6 @@ CREATE TABLE [Product].[Product] (
     Product_Name VARCHAR(255),
     Product_Description TEXT,
     Price DECIMAL(10, 2),
-    Discount DECIMAL(4, 2),
     Product_Category VARCHAR(255),
     Product_Quantity INT,
     Product_Sold INT,
@@ -58,7 +56,6 @@ CREATE TABLE [Account].[Customer] (
     Customer_ID INT PRIMARY KEY,
     Account_ID INT,
     Product_ID INT,
-    Reward_Points INT,
     FOREIGN KEY (Account_ID) REFERENCES [Account].[Account](Account_ID),
     FOREIGN KEY (Product_ID) REFERENCES [Product].[Product](Product_ID)
 )
@@ -83,45 +80,44 @@ CREATE TABLE [OrderManagement].[Order] (
     Order_Date DATE,
     Order_Status VARCHAR(20),
     Shipping_Fee DECIMAL(10, 2),
-    Payment_ID INT,
     Order_Purchase_TimeStamp DATETIME,
-    Order_Canceled DateTIME,
+    Order_Canceled DATETIME,
     Order_Delivered_Carrier_Date DATETIME,
     Order_Delivered_Customer_Date DATETIME,
-    Order_Estimated_Customer_Date DATETIME
+    Order_Estimated_Customer_Date DATETIME,
+    FOREIGN KEY (Customer_ID) REFERENCES [Account].[Customer],
 );
 
 CREATE TABLE [OrderManagement].[Order_Items] (
-    Order_ID INT,
     Order_Item_ID INT PRIMARY KEY,
+    Order_ID INT,
     ProductId INT,
     Product_Name VARCHAR(255),
     Price DECIMAL(10, 2),
     Product_Quantity INT,
-    Cart_ID INT,
     FOREIGN KEY (Order_ID) REFERENCES [OrderManagement].[Order](Order_ID),
-    FOREIGN KEY (Cart_ID) REFERENCES [Cart].[Cart](Cart_ID)
+    FOREIGN KEY (Product_ID) REFERENCES [Product].[Product](Product_ID)
 );
 
 CREATE TABLE [OrderManagement].[Payments] (
-    PaymentID INT PRIMARY KEY,
-    OrderID INT,
+    Payment_ID INT PRIMARY KEY,
+    Order_ID INT,
     Payment_Sequential INT,
     Payment_Type VARCHAR(20),
     Payment_Installments INT,
     Payment_Value DECIMAL(10, 2),
-    FOREIGN KEY (OrderID) REFERENCES [OrderManagement].[Order](Order_ID)
+    FOREIGN KEY (Order_ID) REFERENCES [OrderManagement].[Order](Order_ID)
 );
 
 CREATE TABLE[OrderManagement].[Order_Review] (
     Review_ID INT PRIMARY KEY,
-    OrderID INT,
+    Order_ID INT,
     Review_Score INT,
     Review_Comment_Title VARCHAR(255),
     Review_Comment_Message TEXT,
     Review_Creation_Date DATETIME,
     Review_Answer_Timestamp DATETIME,
-    FOREIGN KEY (OrderID) REFERENCES [OrderManagement].[Order](Order_ID)
+    FOREIGN KEY (Order_ID) REFERENCES [OrderManagement].[Order](Order_ID)
 );
 
 CREATE TABLE [HistoryManagement].[History] (
